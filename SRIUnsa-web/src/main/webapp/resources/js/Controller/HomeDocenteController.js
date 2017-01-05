1aea89901481a4b7e9fa96ad57c170cd10ead31d
innnovacis <innnovacisaqp@gmail.com>
2017-01-05 09:19:56 -0500

    Directiva uppercase, crud de areaorganizacion y crud de tipo de investigador

--- SRIUnsa-web/src/main/webapp/resources/js/Controller/HomeDocenteController.js
+++ SRIUnsa-web/src/main/webapp/resources/js/Controller/HomeDocenteController.js
@@ -81,7 +81,7 @@ function($log, $scope, $location,  TipoInvestigacionService, SharedService,
                 $scope.semestre = value;
             }
         });
-        $scope.getActividades();
+         $scope.getActividades();
     };
     /**************** PAGINACION *****************/
     
@@ -124,7 +124,8 @@ function($log, $scope, $location,  TipoInvestigacionService, SharedService,
     
     if($scope.sharedService.idUsuarioRegistrar === -1){
         console.log("Ingreso como Docente :: ", $scope.sharedService.idUsuarioRegistrar);
-        $scope.GetUsuarioHome($scope.sharedService.idUsuarioRegistrar, 0);
+        $scope.sharedService.idDocente = $scope.sharedService.usuarioLogin.idUsuario;
+        $scope.GetUsuarioHome($scope.sharedService.idDocente, 0);
     } else {
         console.log("Ingreso como director unidad :: ", $scope.sharedService.idUsuarioRegistrar);
         $scope.GetUsuarioHome($scope.sharedService.idUsuarioRegistrar, $scope.sharedService.usuarioLogin.idUsuario);
