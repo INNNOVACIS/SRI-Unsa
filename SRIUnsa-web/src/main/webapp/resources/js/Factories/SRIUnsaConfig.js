65de9aa56f0e55f175f745114b0a6e19e0f07893
innnovacis <innnovacis@innnovacis-Ali>
2017-01-24 10:05:55 -0500

    Actualización de actividades Masivas.

--- SRIUnsa-web/src/main/webapp/resources/js/Factories/SRIUnsaConfig.js
+++ SRIUnsa-web/src/main/webapp/resources/js/Factories/SRIUnsaConfig.js
@@ -6,9 +6,9 @@ investigacionApp.factory("SRIUnsaConfig", function() {
     // Servicio para pasar la URL
     return {
         //Modo Local
-        SRIUnsaUrlServicio: 'http://104.131.8.31:8080/SRIUnsa-web/rest',
+//        SRIUnsaUrlServicio: 'http://104.131.8.31:8080/SRIUnsa-web/rest',
 //        SRIUnsaUrlServicio: 'http://192.168.1.80:8080/SRIUnsa-web/rest',
-//        SRIUnsaUrlServicio: 'http://localhost:8080/SRIUnsa-web/rest',
+        SRIUnsaUrlServicio: 'http://localhost:8080/SRIUnsa-web/rest',
         /*Codigo Actores*/
         DOCE : 1, //Docente Actor
         DIDE : 2, //Director Departamento
