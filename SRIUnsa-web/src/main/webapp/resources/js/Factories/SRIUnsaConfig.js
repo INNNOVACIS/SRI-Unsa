2b0e0e4b10ae618b79e1c821c7d59d37a2a57be8
wilfredoqi <wilfredoqi@gmail.com>
2018-02-09 10:19:56 -0500

    prueba

--- SRIUnsa-web/src/main/webapp/resources/js/Factories/SRIUnsaConfig.js
+++ SRIUnsa-web/src/main/webapp/resources/js/Factories/SRIUnsaConfig.js
@@ -8,8 +8,8 @@ investigacionApp.factory("SRIUnsaConfig", function() {
         //Modo Local
  //         SRIUnsaUrlServicio: 'http://104.131.8.31:8080/SRIUnsa-web/rest',
 //        SRIUnsaUrlServicio: 'http://192.168.1.80:8080/SRIUnsa-web/rest',
-        SRIUnsaUrlServicio: 'http://localhost:8080/SRIUnsa-web/rest',
-//        SRIUnsaUrlServicio: 'http://siri.unsa.edu.pe/SRIUnsa-web/rest',
+//        SRIUnsaUrlServicio: 'http://localhost:8083/SRIUnsa-web/rest',
+        SRIUnsaUrlServicio: 'http://siri.unsa.edu.pe/SRIUnsa-web/rest',
         /*Codigo Actores*/
         DOCE : 1, //Docente Actor
         DIDE : 2, //Director Departamento
